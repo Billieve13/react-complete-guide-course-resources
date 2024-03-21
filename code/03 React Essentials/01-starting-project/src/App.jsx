fd4e240a4d88e3356165b77e377d740773df7843
Billieve13 <rmr837834@gmail.com>
2024-03-21 15:58:58 -0400

    complete through section 3, video 45. Added various props data

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,5 +1,6 @@
 import reactCoreImg from './assets/react-core-concepts.png';
-
+// import componentImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data';
 
 const reactDescriptions = [
   'Fundamental',
@@ -26,12 +27,48 @@ function Header() {
   );
 }
 
+// function CoreConcept(coreValues) { ----same as the code below but with destructuring
+//   return (
+//     <li>
+//       <img src={coreValues.image} alt={coreValues.title} />
+//       <h3>{coreValues.title}</h3>
+//       <p>{coreValues.description}</p>
+//     </li>
+//   );
+// } 
+
+function CoreConcept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">  
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept 
+            {...CORE_CONCEPTS[0]}/>
+            {/* title: {CORE_CONCEPTS[0].title}
+            description: {CORE_CONCEPTS[0].description}
+            image: {CORE_CONCEPTS[0].image}
+            this is the same, but with the spread operator for destructuring */}
+          <CoreConcept 
+            {...CORE_CONCEPTS[1]}/>
+          <CoreConcept 
+            {...CORE_CONCEPTS[2]}/>
+          <CoreConcept 
+            {...CORE_CONCEPTS[3]}/>
+        </ul>
+        </section>
       </main>
     </div>
   );
