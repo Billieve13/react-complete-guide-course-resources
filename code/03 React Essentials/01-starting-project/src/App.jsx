7316ef876b039e9ee2b11c15ada624d4efafea90
Billieve13 <rmr837834@gmail.com>
2024-03-21 15:02:30 -0400

    update site for dynamic language. section 3, video 41

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,12 +1,27 @@
+import reactCoreImg from './assets/react-core-concepts.png';
+
+
+const reactDescriptions = [
+  'Fundamental',
+  'Crucial',
+  'Core',
+]
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(3)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactCoreImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
+      {/* I had to change the number to 3 or else it was never displaying "core" */}
     </header>
   );
 }
